2604b963b590cbd709c09d24e8ed693ff8c92349
Kai-Cai <kaiaschiff@gmail.com>
2018-06-06 15:19:06 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -17,3 +17,17 @@ end
 def platinum_curfew_checker(current_time, curfew_time)
   # code goes here
 end
+
+
+if 4 < 5
+  puts "Four is less than five."
+end
+
+
+if time.hour.now >= 11pm
+  puts "TIME TO GO"
+else 
+  puts "Its ok, you're still under curfew"
+end    
+    
+time.hour.now
